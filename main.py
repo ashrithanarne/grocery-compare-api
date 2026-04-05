fc99dc7f50f6945034531c3f2b9cbdc9d2e42c02
Ashritha Narne <ashrithanarne@gmail.com>
2026-04-05 13:20:53 -0400

    Added a html file that creates a simple frontend in the browser

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ import uvicorn
 from fastapi import FastAPI
 #from typing import List 
 #from pydantic import BaseModel
-import schema
-from typing import List
 from database import engine,SessionLocal
 from models import Base
 from routers import router_stores,router_items
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 app=FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_stores.router)
 app.include_router(router_items.router)
